6098eb28903ccada206d51e1457f9fb2c2eeec77
Ichi <ichi-perm@yandex.ru>
2021-07-16 16:18:38 +0500

    вывод страницы на twig

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
 <?php
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/vendor/autoload.php';
 
+$loader = new \Twig\Loader\FilesystemLoader('template');
+$view = new \Twig\Environment($loader);
+
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+$app->get('/', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('index.twig');
+    $response->getBody()->write($body);
     return $response;
 });
-$app->get('/test', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+$app->get('/about', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('about.twig', ['name'=> 'sd']);
+    $response->getBody()->write($body);
     return $response;
 });
 
